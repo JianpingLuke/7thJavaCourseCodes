4c4e1fa0b504e9728ffc45f87b6fd090ad22b4c6
JianpingLuke <66133511+JianpingLuke@users.noreply.github.com>
2021-11-06 23:28:57 -0400

    2个必做作业
    剩下几个选做暂时没有时间做，代忙完后慢慢做。

---
+++ week1/question1/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+    public static void main(String[] args) {
+        int numInt = 1;
+        short numShort = 1;
+        long numLong = 1;
+        float numFloat = 1;
+        double numDouble = 1;
+        byte numByte = 1;
+        char a = '1';
+        boolean flag = true;
+        for (int i = 0; i < 5; i++) {
+            if (true) {
+                int c = 1 + i;
+                System.out.println(c);
+            }
+        }
+    }
+
+}
